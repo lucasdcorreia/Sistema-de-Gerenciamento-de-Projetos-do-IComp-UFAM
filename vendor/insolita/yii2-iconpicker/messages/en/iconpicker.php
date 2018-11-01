bf3828db1be7b31fe677cdc6d8427f40e14e1678
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-11-01 01:04:11 -0400

    _form relatório prestação

--- vendor/insolita/yii2-iconpicker/messages/en/iconpicker.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-	'CHOOSE_ICON'=>'Choose icon',
-	'SEARCH_ICON'=>'Search icon',
-];
--- vendor/insolita/yii2-iconpicker/messages/ru/iconpicker.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-	'CHOOSE_ICON'=>'Выберите иконку',
-	'SEARCH_ICON'=>'Поиск иконок',
-];
