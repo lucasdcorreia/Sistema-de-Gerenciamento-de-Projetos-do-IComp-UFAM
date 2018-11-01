c776fbf0c45e6e94901da7384b1a3b6d2e593a19
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-11-01 16:47:48 -0400

    Alteração do rótulo projeto

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'U7fDH3H3Y4-AGeIjBZHYbYthrcg-E3nW',
+            'cookieValidationKey' => 'prB2E_AKiRM5AjpMIUSPk4SQ6CThkV3u',
         ],
     ],
 ];
