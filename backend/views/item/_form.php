90d02d91bfedf0236c6cb1c4b5ba691be2e62c2d
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-11-19 18:13:08 -0400

    gerando controller, e views CRUD Item

---
+++ backend/views/item/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Item */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="item-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'natureza')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'valor')->textInput() ?>
+
+    <?= $form->field($model, 'numero_item')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'justificativa')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'quantidade')->textInput() ?>
+
+    <?= $form->field($model, 'custo_unitario')->textInput() ?>
+
+    <?= $form->field($model, 'tipo_item')->textInput() ?>
+
+    <?= $form->field($model, 'descricao')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'id_projeto')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
