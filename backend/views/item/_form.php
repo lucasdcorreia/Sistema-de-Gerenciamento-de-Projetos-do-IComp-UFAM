4395b47e2e5ecb8490b12a33429e3b7a8c69bc46
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-11-19 18:22:58 -0400

    ajuste de termos nos botões

--- backend/views/item/_form.php
+++ backend/views/item/_form.php
@@ -31,7 +31,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'id_projeto')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Salvar', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- backend/views/item/update.php
+++ backend/views/item/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Item */
 
-$this->title = 'Update Item: ' . $model->id;
+$this->title = 'Ulterar Item: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Items', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
