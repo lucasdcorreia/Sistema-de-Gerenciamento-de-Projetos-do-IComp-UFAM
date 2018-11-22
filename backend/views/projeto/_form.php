b7576006ac2bf1761a5490b0db5e6b31dc1ee2f4
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-11-21 20:18:56 -0400

    Adição de Collapses aos formulários de Itens de Projeto

--- backend/views/projeto/_form.php
+++ backend/views/projeto/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\bootstrap\Collapse;
 use yii\widgets\MaskMoney;
 
 /* @var $this yii\web\View */
--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=projetos',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
