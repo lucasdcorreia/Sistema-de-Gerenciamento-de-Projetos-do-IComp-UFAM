211aee0baa601093b3510a9a7edb439e799279c5
alrl <alrl@icomp.ufam.edu.br>
2018-12-04 16:05:46 -0400

    Collapse adicionado aos formulário de Informações financeiras

--- backend/views/orcamento/index.php
+++ backend/views/orcamento/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Orcamentos';
+$this->title = 'Informações financeiras';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="orcamento-index">
@@ -24,82 +24,114 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Orcamento', ['create', 'id_projeto' => $id_projeto], ['class' => 'btn btn-success']) ?>
-    </p>
+    <div class="forms" style="margin-left:25px;">
+      <div class="pull-right">
+          <button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseOrcamento,#collapseValorPago" aria-expanded="false" aria-controls="multiCollapseExample2"
+          style="text-align:left">Expandir tudo</button>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <div class="row" >
+            <p>
+                <a class="btn btn-primary btn-lg" data-toggle="collapse" href="#collapseOrcamento" role="button" aria-expanded="false" aria-controls="multiCollapseExample1"
+                style="width:95%;text-align:left">
+                Orçamento </i>
+                </a>
+            </p>
+            <div class="collapse multi-collapse" id="collapseOrcamento">
+                <div class="card card-body">
+                    <p>
+                        <?= Html::a('Novo', ['create', 'id_projeto' => $id_projeto], ['class' => 'btn btn-success']) ?>
+                    </p>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'id_projeto',
-            'recurso_aprovado',
-            'tipo_de_parcela',
-            'valor_parcela',
-            //'data_recebida',
-            //'valor_receber',
-        ],
-    ]); ?>
+                            'id',
+                            'id_projeto',
+                            'recurso_aprovado',
+                            'tipo_de_parcela',
+                            'valor_parcela',
+                            //'data_recebida',
+                            //'valor_receber',
+                        ],
+                    ]); ?>
+                </div>
+            <hr>
+        </div>
+    </div>
 
-    <p>
-        <?= Html::a('Create Valor Pago', ['valor-pago/create', 'id_projeto' => $id_projeto], ['class' => 'btn btn-success']) ?>
-    </p>
+    <div class="row">
+            <p>
+                <button class="btn btn-primary btn-lg" type="button" data-toggle="collapse" data-target="#collapseValorPago" aria-expanded="false" aria-controls="multiCollapseExample2"
+                style="width:95%;text-align:left">Valor pago</button>
+            </p>
+            <div class="collapse multi-collapse" id="collapseValorPago">
+                <div class="card card-body">
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProviderValorPago,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                    <p>
+                        <?= Html::a('Novo', ['valor-pago/create', 'id_projeto' => $id_projeto], ['class' => 'btn btn-success']) ?>
+                    </p>
+                        <?= GridView::widget([
+                            'dataProvider' => $dataProviderValorPago,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'id_projeto',
-            'numero_ob',
-            'data',
-            'natureza',
-            //'valor',
-            //'tipo',
+                                'id',
+                                'id_projeto',
+                                'numero_ob',
+                                'data',
+                                'natureza',
+                                //'valor',
+                                //'tipo',
 
-            ['class' => 'yii\grid\ActionColumn',
-            'buttons' => [
-                'view' => function ($url, $model) {
-                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
-                                'title' => Yii::t('app', 'Exibir'),
-                    ]);
-                },
-                'update' => function ($url, $model) {
-                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
-                                'title' => Yii::t('app', 'Alterar'),
-                                'data-method' => 'post'
-                    ]);
-                },
-                'delete' => function ($url, $model) {
-                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
-                                'title' => Yii::t('app', 'Excluir'),
-                                'data' => [
-                                                'confirm' => 'Deseja realmente excluir este item?',
-                                                'method' => 'post',
+                                ['class' => 'yii\grid\ActionColumn',
+                                'buttons' => [
+                                    'view' => function ($url, $model) {
+                                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+                                                    'title' => Yii::t('app', 'Exibir'),
+                                        ]);
+                                    },
+                                    'update' => function ($url, $model) {
+                                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                                                    'title' => Yii::t('app', 'Alterar'),
+                                                    'data-method' => 'post'
+                                        ]);
+                                    },
+                                    'delete' => function ($url, $model) {
+                                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                                                    'title' => Yii::t('app', 'Excluir'),
+                                                    'data' => [
+                                                                    'confirm' => 'Deseja realmente excluir este item?',
+                                                                    'method' => 'post',
+                                                    ],
+                                        ]);
+                                    },
                                 ],
-                    ]);
-                },
-            ],
-            'urlCreator' => function ($action, $model, $key, $index) {
-                if ($action === 'view') {
-                    $url ='index.php?r=valor-pago/view&id='.$model->id;
-                    return $url;
-                }
+                                'urlCreator' => function ($action, $model, $key, $index) {
+                                    if ($action === 'view') {
+                                        $url ='index.php?r=valor-pago/view&id='.$model->id;
+                                        return $url;
+                                    }
 
-                if ($action === 'update') {
-                    $url ='index.php?r=valor-pago/update&id='.$model->id;
-                    return $url;
-                }
-                if ($action === 'delete') {
-                    $url ='index.php?r=valor-pago/delete&id='.$model->id;
-                    return $url;
-                }
+                                    if ($action === 'update') {
+                                        $url ='index.php?r=valor-pago/update&id='.$model->id;
+                                        return $url;
+                                    }
+                                    if ($action === 'delete') {
+                                        $url ='index.php?r=valor-pago/delete&id='.$model->id;
+                                        return $url;
+                                    }
 
-            }
-            ]
-        ],
-    ]); ?>
+                                }
+                                ]
+                            ],
+                        ]); ?>
+                    </div>
+                <hr>
+            </div>
+        </div>
 </div>
